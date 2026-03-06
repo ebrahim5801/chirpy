20c80870192dd066749956c288556c059a55f9b9
Ebrahim <ebrahimhanna@wecansync.com>
2026-03-06 14:00:18 +0200

    Chapter 2: routing

--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
+	apiCfg := &apiConfig{}
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/healthz", appHandler)
+	mux.HandleFunc("GET /healthz", appHandler)
 
 	fileServer := http.StripPrefix(
 		"/app",
 		http.FileServer(http.Dir(".")),
 	)
-	mux.Handle("/app/", fileServer)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServer))
+	mux.HandleFunc("GET /metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("POST /reset", apiCfg.resetHandler)
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -26,6 +36,24 @@ func main() {
 func appHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-
 	io.WriteString(w, "OK")
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
 }
